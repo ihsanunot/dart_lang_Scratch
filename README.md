597787ccaec467b46fd4ac1f69f6022a35adccff
“ihsanunot” <ihsanunot@gmail.com>
2023-06-01 22:09:57 +0700

    bikin icon nav sederhana

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 A new Flutter project.
 
-## Getting Started
+## Perbedaan double.infinity & MediaQuery
 
-Buat berkas baru bernama article.dart. Kemudian tambahkan beberapa propertinya. Sesuaikan properti ini dengan berkas json Anda.
+The difference can be summarized into:
+
+I want to be as big as **my parent allows** (double.infinity)
+
+I want to be as big as **the screen** (MediaQuery).
+
+
+## Container
+
+Container widget pada flutter merupakan “Single Child Widget” yang berarti hanya dapat memiliki satu buah child widget saja. 
+
+Akan tetapi dalam sebuah container kita dapat menempatkan row, column, text atau bahkan container lain. Container widget juga dapat dijadikan sebagai dasar dan serangkaian awal dari suatu layout aplikasi.
+
+### Referensi :
+- https://stackoverflow.com/questions/54489513/whats-the-difference-between-double-infinity-and-mediaquery
+- https://educity.app/flutter/how-to-display-images-from-internet-in-flutter
--- lib/main.dart
+++ lib/main.dart
@@ -15,25 +15,65 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const TestDepanDasar(),
+      home: SafeArea(child: MyScaffold()),
     );
   }
 }
 
 class TestDepanDasar extends StatelessWidget {
-  const TestDepanDasar({Key? key}) : super(key: key);
+  const TestDepanDasar({Key? key, required this.title}) : super(key: key);
+
+  final Widget title;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'Testing',
-          style: TextStyle(
-            color: Colors.blue,
-            fontSize: 64,
+    return Container(
+      height: 64,
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blueAccent),
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: null,
           ),
-        ),
+          // Expanded expands its child
+          // to fill the available space.
+          Expanded(child: title),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: null,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+  const MyScaffold({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(
+        children: [
+          // Testing ganti MyAppBar
+          TestDepanDasar(
+            title: Text(
+              'App Testing 1',
+              style: Theme.of(context).primaryTextTheme.titleLarge,
+            ),
+          ),
+          Expanded(
+              child: Center(
+            child: Text(
+              'Hello World',
+              style: TextStyle(
+                fontSize: 42,
+              ),
+            ),
+          ))
+        ],
       ),
     );
   }
